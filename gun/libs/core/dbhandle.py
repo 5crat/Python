c6d357569be0b77f515ac00f210a1f01dcc1c685
root <scrat.r00t@gmail.com>
2016-04-08 18:13:21 +0800

    修改dbhandle

--- gun/libs/core/dbhandle.py
+++ gun/libs/core/dbhandle.py
@@ -23,12 +23,32 @@ class DBhandle(object):
             )
         self.cur = self.conn.cursor()
 
-    def executeSql(self, sql):
+    def executeSql(self, sql, data):
         try:
-            self.cur.execute(sql)
+            if data:
+                self.cur.execute(sql)
+            else:
+                self.cur.execute(sql, data)
             return self.cur
         except:
             return None
+
+    def insert(self, tableName, dataDict):
+        """
+        :type tableName:str
+        :type dataDict:dict
+        :return :bool
+        """
+        sql = "INSERT INTO "+tableName+" ("
+        for i in dataDict.keys():
+            sql += i + ','
+        sign = len(dataDict) * "%s"
+        sql = sql.rstrip(',') + ") VALUES (" + sign.replace('%s', '%s,').rstrip(',') +")"
+        if self.executeSql(sql, dataDict.values()):
+            return True
+        else:
+            return False
+
     def close(self):
         self.cur.close()
         self.conn.commit()
@@ -39,8 +59,20 @@ class DBhandle(object):
 '''
 
 if __name__ == '__main__':
+    '''
     d = DBhandle()
     f = d.execute('select * from bank where bank_id=1')
     for i in f:
         print i
     d.close()
+        '''
+    dataDict= {'a':'a','b':'a'}
+    tableName = 'xxx'
+
+    sql = "INSERT INTO "+tableName+" ("
+    for i in dataDict.keys():
+        sql += i+','
+
+    sign = len(dataDict) * "%s"
+    sql = sql.rstrip(',') + ") VALUES (" + sign.replace('%s', '%s,').rstrip(',') +")"
+    print sql
